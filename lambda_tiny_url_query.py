088e7be0518192474ce8b28b099c666a56d74e98
zhouj37 <jerry.zhou@dell,com>
2020-03-28 13:29:02 +0800

    success get item from dynamodb

--- lambda_tiny_url_query.py
+++ lambda_tiny_url_query.py
@@ -1,9 +1,20 @@
-import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('myTinyURL-db')
 
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    id = event.get("id")
+    response = table.get_item(Key={'id': id})
+    item = response.get("Item", None)
+    if item:
+        return {
+            'statusCode': 200,
+            'body': item
+        }
+    else:
+        return {
+            'statusCode': 404,
+            'body': "item not found"
+        }
