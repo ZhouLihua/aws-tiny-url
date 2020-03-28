7ffea49e646524ed0c7700dea284f96379bec3d9
zhouj37 <jerry.zhou@dell,com>
2020-03-28 13:53:27 +0800

    success to generate shourt url accrding to uuid

--- lambda_tiny_url_make.py
+++ lambda_tiny_url_make.py
@@ -9,17 +9,35 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('myTinyURL-db')
 
 
+# for each make request, we should assume a unique id for it, and
+# generate a short url for it. base62 algoritm
+def generate_short_url(uuid):
+    bits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    shortURL = ""
+    # for each digit find the base 62
+    while uuid > 0:
+        shortURL += bits[uuid % 62]
+        uuid /= 62
+    return shortURL[:: -1]
+
+
 def lambda_handler(event, context):
     longUrl = event.get("longUrl")
-    id = int(time.time())
+    uid = int(time.time())
+    shortUrl = generate_short_url(uid)
+
     table.put_item(
         Item={
-            'id': id,
+            'id': uid,
             'longUrl': longUrl
         }
     )
 
     return {
         'statusCode': 200,
-        'body': id
+        'body': {
+            'shortUrl': shortUrl,
+            "id": uid
+            }
+        
     }
