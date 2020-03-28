0d13172904670b3dc67d5aed607ed35c2efcf7d7
zhouj37 <jerry.zhou@dell,com>
2020-03-28 14:03:19 +0800

    success to decode short url and get longurl from dynamodb

--- lambda_tiny_url_query.py
+++ lambda_tiny_url_query.py
@@ -4,10 +4,23 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('myTinyURL-db')
 
 
+# opposite of generate_short_uri
+def decode_short_url(short_url):
+    bits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    uid = 0
+    length = len(short_url)
+    for key, val in enumerate(short_url):
+        index = bits.index(val)
+        uid += index * pow(62, length - key - 1)
+    return uid
+
+
 def lambda_handler(event, context):
-    id = event.get("id")
-    response = table.get_item(Key={'id': id})
+    shortUrl = event.get("shortUrl")
+    uuid = decode_short_url(shortUrl)
+    response = table.get_item(Key={'id': uuid})
     item = response.get("Item", None)
+    item["shortUrl"] = shortUrl
     if item:
         return {
             'statusCode': 200,
