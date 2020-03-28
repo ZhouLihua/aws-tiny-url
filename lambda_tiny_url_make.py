5062c847935f31eeefd5914e7d26e653e9a4900e
zhouj37 <jerry.zhou@dell,com>
2020-03-28 13:41:40 +0800

    success to put item in dynamodb

--- lambda_tiny_url_make.py
+++ lambda_tiny_url_make.py
@@ -1,8 +1,25 @@
-import json
+import boto3
+import time
+
+
+# db schema
+# partition key: id, type number
+# attribute: longUrl, type string
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('myTinyURL-db')
+
 
 def lambda_handler(event, context):
-    # TODO implement
+    longUrl = event.get("longUrl")
+    id = int(time.time())
+    table.put_item(
+        Item={
+            'id': id,
+            'longUrl': longUrl
+        }
+    )
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': id
     }
